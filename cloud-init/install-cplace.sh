e8672fefb964545a74c588b883c974f28cc21968
Sebastian W <sebastian.weitzel@collaboration-factory.de>
2021-12-05 17:33:47 +0100

    Fix cplace proxy config
    Signed-off-by: Sebastian W <sebastian.weitzel@collaboration-factory.de>

--- cloud-init/install-cplace.sh
+++ cloud-init/install-cplace.sh
@@ -4,13 +4,13 @@ set -e
 # configure http proxy system-wide
 cat > /etc/profile.d/proxy.sh << EOT
 PROXY_URL="http://10.77.1.3:3128/"
-export http_proxy="$PROXY_URL"
-export https_proxy="$PROXY_URL"
-export ftp_proxy="$PROXY_URL"
+export http_proxy="\$PROXY_URL"
+export https_proxy="\$PROXY_URL"
+export ftp_proxy="\$PROXY_URL"
 export no_proxy="127.0.0.1,localhost"
-export HTTP_PROXY="$PROXY_URL"
-export HTTPS_PROXY="$PROXY_URL"
-export FTP_PROXY="$PROXY_URL"
+export HTTP_PROXY="\$PROXY_URL"
+export HTTPS_PROXY="\$PROXY_URL"
+export FTP_PROXY="\$PROXY_URL"
 export NO_PROXY="127.0.0.1,localhost"
 EOT
 
