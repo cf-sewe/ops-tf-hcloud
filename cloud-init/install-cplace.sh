c0dd4eb7a9a6b6e060c4898a0285fb4ee420ed76
Sebastian W <sebastian.weitzel@collaboration-factory.de>
2021-12-05 16:16:52 +0100

    Improve cplace install script
    Signed-off-by: Sebastian W <sebastian.weitzel@collaboration-factory.de>

--- cloud-init/install-cplace.sh
+++ cloud-init/install-cplace.sh
@@ -2,7 +2,7 @@
 set -e
 
 # configure http proxy system-wide
-cat > /etc/profile.d/squid.sh << EOT
+cat > /etc/profile.d/proxy.sh << EOT
 PROXY_URL="http://10.77.1.3:3128/"
 export http_proxy="$PROXY_URL"
 export https_proxy="$PROXY_URL"
@@ -15,7 +15,7 @@ export NO_PROXY="127.0.0.1,localhost"
 EOT
 
 # use proxy for package manager
-cat > /etc/dnf/dnf.conf << EOT
+cat >> /etc/dnf/dnf.conf << EOT
 proxy=http://10.77.1.3:3128
 EOT
 
