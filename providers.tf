f7974fc8881c7228762d2042e6a477846ce93d7f
Sebastian W <sebastian.weitzel@collaboration-factory.de>
2021-12-11 11:08:44 +0100

    Fix attempt 5
    Signed-off-by: Sebastian W <sebastian.weitzel@collaboration-factory.de>

--- providers.tf
+++ providers.tf
@@ -6,9 +6,10 @@ provider "hcloud" {
 # Azure
 provider "azurerm" {
   features {}
-  environment     = "public"
-  subscription_id = var.azurerm_subscription_id
-  tenant_id       = var.azurerm_tenant_id
-  client_id       = var.azurerm_client_id
-  client_secret   = var.azurerm_client_secret
+  environment                = "public"
+  subscription_id            = var.azurerm_subscription_id
+  tenant_id                  = var.azurerm_tenant_id
+  client_id                  = var.azurerm_client_id
+  client_secret              = var.azurerm_client_secret
+  skip_provider_registration = true
 }
